3ce55b34a422a2ac2dbed0bdacebf6c5988178b8
Dilar Gelenava <d.gelenava@noxtton.com>
2022-10-26 01:02:20 +0400

    feat: remove note

--- app/src/main/java/com/example/android_assignment_4/NoteActivity.kt
+++ app/src/main/java/com/example/android_assignment_4/NoteActivity.kt
@@ -1,44 +1,46 @@
 package com.example.android_assignment_4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 
 
 class NoteActivity : AppCompatActivity() {
 
-    lateinit var noteLayout: LinearLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.note_item)
-
-        noteLayout = findViewById(R.id.noteLayout)
-        noteLayout.setOnClickListener() {
-            deleteNote(this)
-        }
     }
 
 
     private fun deleteNote(view: NoteActivity) {
-        val mPrefs = getPreferences(MODE_PRIVATE)
-        val gson = Gson()
-        val json = mPrefs.getString("notes", "[]")
-        val notes: Array<Note> = gson.fromJson(json, Array<Note>::class.java)
-        val noteId = intent.getIntExtra("noteId", 0)
-        val newNotes = (notes.filter { it.id != noteId }).toTypedArray()
+//        val mPrefs = getPreferences(MODE_PRIVATE)
+//        val gson = Gson()
+//        val json = mPrefs.getString("notes", "[]")
+//        val notes: Array<Note> = gson.fromJson(json, Array<Note>::class.java)
+//        val noteId = intent.getIntExtra("noteId", 0)
+//        val newNotes = (notes.filter { it.id != noteId }).toTypedArray()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val manager = LinearLayoutManager(this)
-        recyclerView.layoutManager = manager
-        recyclerView.setHasFixedSize(true)
-        val adapter = NoteRecyclerAdapter(newNotes)
-        recyclerView.adapter = adapter
+//        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+//        val manager = LinearLayoutManager(this)
+//        recyclerView.layoutManager = manager
+//        recyclerView.setHasFixedSize(true)
+//        val adapter = NoteRecyclerAdapter(newNotes)
+//        recyclerView.adapter = adapter
 
-        val newJson = gson.toJson(newNotes)
-        mPrefs.edit().putString("notes", newJson).apply()
+//        val newJson = gson.toJson(newNotes)
+//        mPrefs.edit().putString("notes", newJson).apply()
+
+//        val mainLayout = findViewById<View>(R.id.mainLayout)
+//
+//        val snackbar = Snackbar.make(mainLayout, "Note deleted", Snackbar.LENGTH_LONG)
+//        snackbar.show()
     }
+
 }
--- app/src/main/java/com/example/android_assignment_4/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/example/android_assignment_4/NoteRecyclerAdapter.kt
@@ -1,10 +1,17 @@
 package com.example.android_assignment_4
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 
 class NoteRecyclerAdapter(private val notes: Array<Note>) :
     RecyclerView.Adapter<NoteRecyclerAdapter.NoteViewHolder>() {
@@ -14,9 +21,24 @@ class NoteRecyclerAdapter(private val notes: Array<Note>) :
         return NoteViewHolder(itemView)
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = notes[position]
         holder.setData(currentNote)
+        holder.noteLayout.setOnClickListener() {
+            val mPrefs =
+                holder.itemView.context.getSharedPreferences("shared preferences", MODE_PRIVATE)
+            val gson = Gson()
+            val json = mPrefs.getString("notes", "[]")
+            val notes: Array<Note> = gson.fromJson(json, Array<Note>::class.java)
+            val newNotes = notes.filter { it.id != currentNote.id }
+            val newJson = gson.toJson(newNotes)
+            mPrefs.edit().putString("notes", newJson).apply()
+            notifyItemRemoved(position)
+
+            val snackbar = Snackbar.make(holder.itemView, "Note deleted", Snackbar.LENGTH_LONG)
+            snackbar.show()
+        }
     }
 
     override fun getItemCount() = notes.size
@@ -25,10 +47,12 @@ class NoteRecyclerAdapter(private val notes: Array<Note>) :
         private val idView: TextView = itemView.findViewById(R.id.id)
         private val titleView: TextView = itemView.findViewById(R.id.title)
         private val descriptionView: TextView = itemView.findViewById(R.id.description)
+        val noteLayout: View = itemView.findViewById(R.id.noteLayout)
         fun setData(note: Note) {
             titleView.text = note.title
             descriptionView.text = note.description
             idView.text = note.id.toString()
+            noteLayout.id = note.id
         }
     }
 }
